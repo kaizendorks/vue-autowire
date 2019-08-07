877211b1885e3d9ff0c9ee32017a946b6fbc8a10
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2019-08-07 15:34:30 +0100

    prevent error with require.context.id being an int in production bundles

--- src/autowire.js
+++ src/autowire.js
@@ -95,7 +95,7 @@ function registerComponents (Vue, requireContext) {
  */
 function registerAsyncComponents (Vue, requireContext) {
   // Make sure require.context was created with lazy mode
-  if (!requireContext.id.includes('lazy')) {
+  if (typeof requireContext.id === 'string' && !requireContext.id.includes('lazy')) {
     throw new Error('require.context for async components should be created in lazy mode. See https://github.com/webpack/docs/wiki/context#context-module-api');
   }
 
